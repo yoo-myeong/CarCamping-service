e9c45abdc094dccdb369f15edee949f3c30317fc
yoo-myeong <yoou201@naver.com>
2021-11-08 18:58:00 +0900

    create client main page
    ejs 조립식 설계
    epxress static 파일 경로 설정

---
+++ client/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import weatherRouter from "./router/weather.router.js";
+import morgan from "morgan";
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
+app.use("/weather", weatherRouter);
+app.use("/", (req, res, next) => {
+  res.render("main.ejs");
+});
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.sendStatus(500);
+});
+
+app.listen(5000);
