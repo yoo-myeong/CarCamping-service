184c5190aa5fbe838307a7c837833bf8b87d0082
yoo-myeong <yoou201@naver.com>
2021-12-12 21:02:30 +0900

    bug : map readonly required
    readonly가 설정되면 required가 동작하지 않아서 JS로 검증

--- client/public/js/story/storyPost.js
+++ client/public/js/story/storyPost.js
@@ -15,10 +15,15 @@ function checkImgCnt() {
 
 const buttonOuttaForm = $("#button-outta-form");
 buttonOuttaForm.click(() => {
-  const formButton = $("#form-button");
-  if (checkImgCnt()) {
-    formButton.trigger("click");
+  if (document.querySelector("#form-address").value) {
+    const formButton = $("#form-button");
+    if (checkImgCnt()) {
+      formButton.trigger("click");
+    } else {
+      alert("업로드 가능한 이미지 개수는 최대 5개 입니다.");
+    }
   } else {
-    alert("업로드 가능한 이미지 개수는 최대 5개 입니다.");
+    // 주소 입력창은 readonly가 적용되어 required를 사용할 수 없어서 JS로 검증
+    alert("주소를 입력해주세요.");
   }
 });
