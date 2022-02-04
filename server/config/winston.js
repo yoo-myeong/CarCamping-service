fbdacb9116d785a8c4447a477f53a0b96f446844
yoo-myeong <yoou201@naver.com>
2022-02-04 11:14:26 +0900

    winston 적용

---
+++ server/config/winston.js
@@ -0,0 +1,47 @@
+import winston from "winston";
+import winstonDaily from "winston-daily-rotate-file";
+
+const logDir = "logs";
+const { combine, timestamp, printf, colorize, splat } = winston.format;
+
+function logFormat() {
+  return (info) => {
+    return `[${info.level}]: ${info.timestamp} - ${info.message}`;
+  };
+}
+
+export const logger = winston.createLogger({
+  format: combine(timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), printf(logFormat())),
+  transports: [
+    new winstonDaily({
+      level: "info",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+    new winstonDaily({
+      level: "error",
+      datePattern: "YYYY-MM-DD",
+      dirname: logDir + "/error",
+      filename: `%DATE%.error.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: combine(splat(), colorize({ all: true })),
+    })
+  );
+}
+
+logger.stream = {
+  write: (message) => {
+    logger.info(message);
+  },
+};
