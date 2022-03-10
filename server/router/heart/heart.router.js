aa8a94489bf2ecb03cd7c1a7647efe367dac956e
yoo-myeong <yoou201@naver.com>
2022-03-10 21:57:43 +0900

    heart API 버그 : 도메인이 변경되면서 프론트엔드 JS 코드에서 버그 발생.

--- server/router/heart/heart.router.js
+++ server/router/heart/heart.router.js
@@ -4,10 +4,10 @@ import { isAuth } from "../../middleware/isAuth.js";
 const router = express.Router();
 
 export default function heartRouter(heartController) {
+  router.get("/stories", isAuth, heartController.getStoryOrderedByHeart);
   router.get("/:storyId", isAuth, heartController.getHeart);
   router.post("/", isAuth, heartController.createHeart);
   router.delete("/:storyId", isAuth, heartController.deleteHeart);
-  router.get("/stories", isAuth, heartController.getStoryOrderedByHeart);
 
   return router;
 }
